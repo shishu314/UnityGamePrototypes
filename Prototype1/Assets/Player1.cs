50f60ea06599cff8793134e59a8783bcd6a78366
Shi Shu <ss10198@nyu.edu>
2019-02-01 16:52:30 -0500

    Prototype for a game of tag created

---
+++ Prototype1/Assets/Player1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    [SerializeField]
+    private Timer timer;
+    private Rigidbody2D body;
+    [SerializeField]
+    private float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        HandleMovement(horizontal, vertical);   
+    }
+
+    private void HandleMovement(float horizontal, float vertical)
+    {
+        Vector2 force = new Vector2(horizontal * movementSpeed, vertical * movementSpeed);
+        body.AddForce(force);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Player2" && !timer.Player1Turn && !timer.GameOver)
+        {
+            Destroy(gameObject);
+            timer.GameOver = true;
+        }
+    }
+}
---
+++ Prototype1/Assets/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public bool Player1Turn { get; set; }
+    public bool GameOver { get; set; } = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1Turn = UnityEngine.Random.value > 0.5;
+        InvokeRepeating("ChangeTurn", 0, 10);
+    }
+
+    void ChangeTurn()
+    {
+        Player1Turn = !Player1Turn;
+        Color32 color = Player1Turn ? new Color32(29, 188, 212, 255) : new Color32(69, 209, 21, 255);
+        GetComponent<SpriteRenderer>().color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
