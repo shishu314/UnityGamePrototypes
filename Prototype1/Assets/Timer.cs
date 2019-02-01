3c7aee3108a0d2357af8e15f1642acc24e282e53
Shi Shu <ss10198@nyu.edu>
2019-02-01 18:15:54 -0500

    Circle changes at a random interval

--- Prototype1/Assets/Timer.cs
+++ Prototype1/Assets/Timer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,15 +9,17 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Player1Turn = UnityEngine.Random.value > 0.5;
-        InvokeRepeating("ChangeTurn", 0, 10);
+        Player1Turn = Random.value > 0.5;
+        Invoke("ChangeTurn", 0);
     }
 
     void ChangeTurn()
     {
+        float randomTime = Random.Range(3, 10);
         Player1Turn = !Player1Turn;
         Color32 color = Player1Turn ? new Color32(29, 188, 212, 255) : new Color32(69, 209, 21, 255);
         GetComponent<SpriteRenderer>().color = color;
+        Invoke("ChangeTurn", randomTime);
     }
 
     // Update is called once per frame
