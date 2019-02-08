e715953688c7b71fda8e95bd84fdec055559f877
Shi Shu <ss10198@nyu.edu>
2019-02-07 23:12:25 -0500

    Began working on prototype 2

---
+++ Prototype2/Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public Sprite[] polygons;
+    private int Sides { get; set; } = 4;
+    private SpriteRenderer Renderer { get; set; }
+    private Vector2 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(Sides > 3)
+            {
+                --Sides;
+                Renderer.sprite = polygons[Sides];
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.deltaTime * 5);
+    }
+}
