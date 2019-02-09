80d2114fd47dcbc8cee593122f59f48f8ba1b3fa
Shi Shu <ss10198@nyu.edu>
2019-02-08 21:52:50 -0500

    spawning works

---
+++ Prototype2/Assets/SpawnEntities.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEntities : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject LoseSide;
+    [SerializeField]
+    public GameObject GainSide;
+    public float spawnTime;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > spawnTime)
+        {
+            SpawnRandom();
+            timer = 0;
+        }
+    }
+
+    public void SpawnRandom()
+    {
+        Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Screen.height, Camera.main.farClipPlane / 2));
+        Instantiate(GainSide, screenPosition, Quaternion.identity);
+    }
+}
