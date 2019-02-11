8e0ae4f9d7bf771af2707bc87c2b9de74491ce5c
Shi Shu <ss10198@nyu.edu>
2019-02-11 14:52:27 -0500

    Toggling reverse direction indicator working

--- Prototype2/Assets/SpawnEntities.cs
+++ Prototype2/Assets/SpawnEntities.cs
@@ -6,11 +6,14 @@ public class SpawnEntities : MonoBehaviour
 {
     public GameObject LoseSide;
     public GameObject GainSide;
+    public GameObject[] SpawnDirections;
     public Player Player;
     public float spawnTime;
     private float spawnTimer = 0;
-    private float reverseTimer = 0;
     public float reverseTime;
+    private float reverseTimer = 0;
+    public float toggleTime;
+    private float toggleTimer;
     private bool Reverse { get; set; } = false;
     // Start is called before the first frame update
     void Start()
@@ -28,11 +31,49 @@ public class SpawnEntities : MonoBehaviour
             spawnTimer = 0;
         }
         reverseTimer += Time.deltaTime;
+        if(reverseTimer > reverseTime - 3.0)
+        {
+            toggleTimer += Time.deltaTime;
+            if (toggleTimer > toggleTime)
+            {
+                for (var i = 0; i < SpawnDirections.Length; ++i)
+                {
+                    ToggleDrawing(ref SpawnDirections[i]);
+                }
+                toggleTimer = 0;
+            }
+        }
         if (reverseTimer > reverseTime)
         {
             Reverse = !Reverse;
             reverseTimer = 0;
+            toggleTimer = 0;
+            for (var i = 0; i < SpawnDirections.Length; ++i)
+            {
+                FlipY(ref SpawnDirections[i]);
+                SetDrawing(ref SpawnDirections[i], true);
+            }
         }
+    }
+
+    private void FlipY(ref GameObject obj)
+    {
+        var transform = obj.GetComponent<Transform>();
+        var yFlip = transform.localScale;
+        yFlip.y *= -1;
+        transform.localScale = yFlip;
+    }
+
+    private void SetDrawing(ref GameObject obj, bool draw)
+    {
+        var renderer = obj.GetComponent<SpriteRenderer>();
+        renderer.enabled = draw;
+    }
+
+    private void ToggleDrawing(ref GameObject obj)
+    {
+        var renderer = obj.GetComponent<SpriteRenderer>();
+        renderer.enabled = !renderer.enabled;
     }
 
     public void SpawnRandom()
@@ -44,10 +85,7 @@ public class SpawnEntities : MonoBehaviour
         if(Reverse)
         {
             obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            var transform = obj.GetComponent<Transform>();
-            var yFlip = transform.localScale;
-            yFlip.y *= -1;
-            obj.GetComponent<Transform>().localScale = yFlip;
+            FlipY(ref obj);
         }
     }
 }
