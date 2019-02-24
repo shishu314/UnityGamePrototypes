187d3f39f93a052996c13c4b9a8982acaf73c3e4
Shi Shu <ss10198@nyu.edu>
2019-02-24 13:18:12 -0500

    Began working on prototype 3

---
+++ Prototype3/Assets/Player1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float movementSpeed;
+    public Sprite[] sprites;
+    public bool charge;
+    private bool onGround = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = charge ? sprites[0] : sprites[1];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        Vector2 force = new Vector2(horizontal * movementSpeed, 0);
+        body.AddForce(force);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        var collidedBody = col.gameObject.GetComponent<Rigidbody2D>();
+        var collidedObjectPosition = col.gameObject.GetComponent<Transform>().position;
+        var gameObjPosition = gameObject.GetComponent<Transform>().position;
+        if (collidedBody.bodyType == RigidbodyType2D.Static && collidedObjectPosition.y < gameObjPosition.y)
+        {
+            onGround = true;
+        }
+    }
+
+    private void Jump()
+    {
+        if(onGround)
+        {
+            body.AddForce(new Vector2(0, body.mass * 5), ForceMode2D.Impulse);
+            onGround = false;
+        }
+    }
+}
