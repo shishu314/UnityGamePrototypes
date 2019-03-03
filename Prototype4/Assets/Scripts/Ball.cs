ee3a16bc2d39df43c7721b548eb5b9d3009eb665
Shi Shu <ss10198@nyu.edu>
2019-03-02 21:40:38 -0500

    Let the countdown begin

--- Prototype4/Assets/Scripts/Ball.cs
+++ Prototype4/Assets/Scripts/Ball.cs
@@ -7,37 +7,61 @@ public class Ball : MonoBehaviour
     Vector2 Direction;
     public float speed;
     float radius;
+    public int numCollisionLeft;
+    public GameObject textFab;
+    GameObject text;
     // Start is called before the first frame update
     void Start()
     {
         Direction = Vector2.one.normalized;
         radius = transform.localScale.x / 2;
+        text = Instantiate(textFab);
+        text.GetComponent<MeshRenderer>().sortingLayerName = "Text";
+        text.transform.Translate(new Vector2(radius/2 * -1, radius/2));
+        text.GetComponent<TextMesh>().text = numCollisionLeft.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool Collided = false;
         transform.Translate(Direction * speed * Time.deltaTime);
+        text.transform.Translate(Direction * speed * Time.deltaTime);
 
         if (transform.position.y < GameManager.bottomLeft.y + radius && Direction.y < 0)
         {
             Direction.y *= -1;
+            Collided = true;
         }
 
         if (transform.position.y > GameManager.topRight.y - radius / 2 && Direction.y > 0)
         {
             Direction.y *= -1;
+            Collided = true;
         }
 
         if (transform.position.x < GameManager.bottomLeft.x + radius && Direction.x < 0)
         {
             Direction.x *= -1;
+            Collided = true;
         }
 
         if (transform.position.x > GameManager.topRight.x - radius / 2 && Direction.x > 0)
         {
             Direction.x *= -1;
+            Collided = true;
         }
+
+        if(Collided)
+        {
+            numCollisionLeft -= 1;
+            text.GetComponent<TextMesh>().text = numCollisionLeft.ToString();
+        }
+    }
+
+    void Explode()
+    {
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -48,11 +72,15 @@ public class Ball : MonoBehaviour
             if (isRight && Direction.x > 0)
             {
                 Direction.x *= -1;
+                numCollisionLeft -= 1;
+                text.GetComponent<TextMesh>().text = numCollisionLeft.ToString();
             }
 
             if (!isRight && Direction.x < 0)
             {
                 Direction.x *= -1;
+                numCollisionLeft -= 1;
+                text.GetComponent<TextMesh>().text = numCollisionLeft.ToString();
             }
         }
     }
