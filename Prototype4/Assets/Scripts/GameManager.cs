448962f41987a916ac2b03a37e2273bb92b467f1
Shi Shu <ss10198@nyu.edu>
2019-03-23 20:47:06 -0400

    Began working on prototype 5

--- Prototype4/Assets/Scripts/GameManager.cs
+++ Prototype4/Assets/Scripts/GameManager.cs
@@ -9,6 +9,9 @@ public class GameManager : MonoBehaviour
 
     public static Vector2 bottomLeft;
     public static Vector2 topRight;
+
+    public int start;
+    public int end;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,7 @@ public class GameManager : MonoBehaviour
         topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
 
         Ball Ball = Instantiate(ball);
-        Ball.numCollisionLeft = Random.Range(20, 70);
+        Ball.numCollisionLeft = Random.Range(start, end);
         Paddle Paddle1 = Instantiate(paddle);
         Paddle Paddle2 = Instantiate(paddle);
         Paddle1.Init(true);
---
+++ Prototype5/Assets/InitializeStage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeStage : MonoBehaviour
+{
+    public GameObject tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(var i = -5; i < 5; ++i)
+        {
+            for (var j = -5; j < 5; ++j)
+            {
+                Instantiate(tile, new Vector3(i * 0.5F, j * 0.5F, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
