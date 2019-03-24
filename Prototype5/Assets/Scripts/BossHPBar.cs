f66eab4bdf8d3691d9a6b532854b3e46102e7ef9
Shi Shu <ss10198@nyu.edu>
2019-03-24 16:24:24 -0400

    Added ghidora

--- Prototype5/Assets/Scripts/BossHPBar.cs
+++ Prototype5/Assets/Scripts/BossHPBar.cs
@@ -18,6 +18,6 @@ public class BossHPBar : MonoBehaviour
         var transform = GetComponent<Transform>();
         var newScale = (HP / TotalHP) * 10;
         transform.localScale = new Vector3(newScale, 1, 1);
-        transform.position = new Vector3((newScale - 10) /2, 4, 0);
+        transform.position = new Vector3((newScale - 10) /2, 4.4f, 0);
     }
 }
