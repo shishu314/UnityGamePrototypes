e7c2fdc4f73fe9773b7219b0de911f59c94d8c9a
Shi Shu <ss10198@nyu.edu>
2019-03-28 22:52:20 -0400

    Player faces different positions based on mouse

---
+++ Prototype6/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Crosshair crosshair;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var crossHairX = crosshair.transform.position.x;
+        var playerX = transform.position.x;
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.flipX = playerX > crossHairX;
+    }
+}
