a9d2996f9d9d1d249431dc8e492e2b53d2f3a0d2
Shi Shu <ss10198@nyu.edu>
2019-03-28 22:40:24 -0400

    Began prototype6, implemented crosshair movement

---
+++ Prototype6/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z)
+        {
+            z = 10.0f //distance of the plane from the camera
+        };
+        transform.position = Camera.main.ScreenToWorldPoint(screenPoint);
+    }
+}
