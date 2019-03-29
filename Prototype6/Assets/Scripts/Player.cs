d5ef2e84adbb10ed2b2822deaf230be0879338dc
Shi Shu <ss10198@nyu.edu>
2019-03-28 23:16:57 -0400

    Player movement done, began working on jumping

--- Prototype6/Assets/Scripts/Player.cs
+++ Prototype6/Assets/Scripts/Player.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Crosshair crosshair;
+    public float movementSpeed;
+    public float jumpPower;
+    private Rigidbody2D body;
+    private bool onGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         var crossHairX = crosshair.transform.position.x;
         var playerX = transform.position.x;
         var spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.flipX = playerX > crossHairX;
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (onGround)
+            {
+                body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                onGround = false;
+            }
+        }
+    }
+
+    private void HandleMovement(float horizontal)
+    {
+        Vector2 force = new Vector2(horizontal * movementSpeed, 0);
+        body.AddForce(force);
     }
 }
