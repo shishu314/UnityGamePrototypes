aae483a463c112c6361b47d5be010f7ddb957381
Shi Shu <ss10198@nyu.edu>
2019-04-06 11:20:50 -0400

    Arrows vibrate when they get stuck

--- Prototype7/Assets/Scripts/Arrow.cs
+++ Prototype7/Assets/Scripts/Arrow.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
+    public Vector3 stuckPosition;
+    public int vibrateCount = 0;
+    public float vibrateTime = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +17,43 @@ public class Arrow : MonoBehaviour
     void Update()
     {
         SetRotation();
+        Vibrate();
     }
 
     void SetRotation()
     {
         var rb = GetComponent<Rigidbody2D>();
-        var velocity = rb.velocity.normalized;
-        var angle = Mathf.Atan2(velocity.y, velocity.x) - 1.0f;
-        transform.localRotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        if (rb.bodyType != RigidbodyType2D.Static)
+        {
+            var velocity = rb.velocity.normalized;
+            var angle = Mathf.Atan2(velocity.y, velocity.x) - 1.0f;
+            transform.localRotation = Quaternion.Euler(0, 0, angle * Mathf.Rad2Deg);
+        }
+    }
+
+    void Vibrate()
+    {
+        var body = GetComponent<Rigidbody2D>();
+        if(body.bodyType == RigidbodyType2D.Static)
+        {
+            if(vibrateTime < 0.3f)
+            {
+                var pos = stuckPosition;
+                if (vibrateCount % 2 == 0)
+                {
+                    pos.y -= 0.02f;
+                } else
+                {
+                    pos.y += 0.02f;
+                }
+                transform.position = pos;
+                vibrateCount += 1;
+                vibrateTime += Time.deltaTime;
+            } else
+            {
+                transform.position = stuckPosition;
+            }
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -30,6 +62,7 @@ public class Arrow : MonoBehaviour
         {
             var body = GetComponent<Rigidbody2D>();
             body.bodyType = RigidbodyType2D.Static;
+            stuckPosition = transform.position;
         }
     }
 }
